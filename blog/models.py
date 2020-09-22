d1be7835c499389130d2a4aa15fcf19a3ba346f2
dave.d <davtyan.d.n@mail.ru>
2020-09-22 23:01:58 +0200

    Blog app added

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+class Blog(models.Model):
+
+	blog_title = models.CharField(max_length=300)
+	blog_date  = models.DateTimeField(auto_now=False, auto_now_add=False)
+	blog_text  = models.TextField()
+	blog_image = models.ImageField(upload_to='event_images/')
